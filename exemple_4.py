92007dce452d99b84d797a6d91a8d90fbf52c4be
Bakashev Sergey <BakashevSergey@gmail.com>
2022-06-15 15:34:13 +0300

    counting from 1 to 20

--- exemple_4.py
+++ exemple_4.py
@@ -4,3 +4,22 @@ for magician in magicians:
     print(magician.title() + ', that was a great trick ')
     print("I can't wait to see your next trick " + magician.title() + '\n')
 print('Thanks for the great show')
+
+#range
+for i in range(1,6):
+    print(i)
+
+#create list for range
+listRang=list(range(1,6))
+print(listRang)
+
+listRangTwo=list(range(2,11,2))
+print(listRangTwo)
+
+square=[]
+for i in range(1,11):
+    square.append(i**2)
+print(square)
+
+squareTwo=[val**2 for val in range(1,11)]
+print(squareTwo)
