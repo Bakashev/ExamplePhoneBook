5e43a61a0b0fe22dcf1cce32aec324560cbe114d
Bakashev Sergey <BakashevSergey@gmail.com>
2022-06-15 14:39:42 +0300

    Сортировка списка

---
+++ exercise_3_8.py
@@ -0,0 +1,25 @@
+# create list
+countryes=['Dominican', 'Cuba','Poland','Paris','Germany']
+#view list
+print(countryes)
+#sorted list not change
+print(sorted(countryes))
+#view list
+print(countryes)
+#reverse list not change
+print(sorted(countryes,reverse=True))
+#view list
+print(countryes)
+#reverse listr whith change
+countryes.reverse()
+#view list
+print(countryes)
+#repeat reverse
+countryes.reverse()
+print(countryes)
+# use sort
+print('use sort')
+countryes.sort()
+print(countryes)
+countryes.sort(reverse=True)
+print(countryes)
