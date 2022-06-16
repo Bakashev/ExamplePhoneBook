8b0f3edad30480e11d208a1f4e5a20258f3560ba
Bakashev Sergey <BakashevSergey@gmail.com>
2022-06-16 18:43:24 +0300

    exercise 6_1

--- alien.py
+++ alien.py
@@ -1,3 +1,39 @@
 alien_0={'color':'green','points':5}
 print(alien_0['color'])
 print(alien_0['points'])
+alien_0['x_position']=0
+alien_0['y_position']=25
+print(alien_0)
+alien_1={}
+alien_1['point']=1
+alien_1['name']='Andrey'
+print(alien_1)
+alien_2={}
+alien_2['color']= 'green'
+print('Alien color ' + alien_2['color'])
+alien_2['color'] = 'red'
+print('Alien color now ' + alien_2['color'])
+alien_3={'x_position':0,'y_position':25,'speed' : 'medium'}
+print('Original position ' + str(alien_3['x_position']))
+if alien_3['speed']=='slow':
+    x_increment = 1
+elif alien_3['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+alien_3['x_position']  += x_increment
+
+print("Next x position " + str(alien_3['x_position']))
+
+del alien_0['points']
+print(alien_0)
+
+favorite_languages={
+    'Sergey' : 'python',
+    'Pavel' : 'java',
+    'Stas' : 'C#',
+    'Viktor' : 'java',
+    'Dmitri' : 'viper'
+    }
+print(favorite_languages)
+print('Viktor favorite languges ' + favorite_languages['Viktor'].title())
---
+++ exercise_6_1.py
@@ -0,0 +1,7 @@
+people={'first_name':'Yulia',
+        'last_name' : 'Bakasheva',
+        'age' : 35,
+        'city':'Minsk'
+        }
+for i in people:
+    print(people[i])
