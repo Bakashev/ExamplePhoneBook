930f8fbe6c86c48b0a5b8907f26da0d396cf68fc
Bakashev Sergey <BakashevSergey@gmail.com>
2022-07-11 22:53:35 +0300

    count word on book

--- exercise_10_10.py
+++ exercise_10_10.py
@@ -1,4 +1,14 @@
-fielname = 'magic.txt'
-with open(fielname) as f_obj:
-    content = f_obj.read()
-    print(content.count('name'))
+def count_word_book(*fielnames,word = 'the'):
+    for fielname in fielnames:
+        try:
+            with open(fielname, encoding='utf-8') as f_obj:
+                content = f_obj.read()
+                #print(content.count(word))
+        except FileNotFoundError:
+            print('Fiel not found!!!')
+        else:
+            print('Book {} have {} word - {}'. format(fielname, content.count(word),word))
+
+#fielname = 'magic.txt'
+count_word_book('Alice.txt','magic.txt','book.txt', 'book_two.txt','little princesse.txt')
+count_word_book('Alice.txt','magic.txt','book.txt', 'book_two.txt','little princesse.txt', word='and')
