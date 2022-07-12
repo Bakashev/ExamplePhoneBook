8b6906cf6498e6a06b86fbbe73851f28279eda25
Bakashev Sergey <BakashevSergey@gmail.com>
2022-07-12 11:26:20 +0300

    add function get_new_username

--- greet_user.py
+++ greet_user.py
@@ -9,6 +9,14 @@ def get_stored_username():
         return None
     else:
         return username
+
+def get_new_username():
+    """Create new user name"""
+    fielname='user_info.json'
+    name = input("Ente your name: ")
+    with open(fielname,'w') as f_obj:
+        json.dump(name,f_obj)
+
 def greet_user ():
     """Use save user if file not found we will create new file and write user name"""
     fielname = 'user_info.json'
@@ -16,8 +24,4 @@ def greet_user ():
     if username:
         print('Well come back {}'.format(username))
     else:
-        with open(fielname,'w') as f_obj:
-
-            name = input('Enter you name: ')
-            json.dump(name,f_obj)
-
+        get_new_username()
