a3cff392dd8fb9f51e6f60ca9683d9dc9f93d048
Bakashev Sergey <BakashevSergey@gmail.com>
2022-07-12 11:20:30 +0300

    add function get_stored_username

--- greet_user.py
+++ greet_user.py
@@ -1,5 +1,23 @@
 import json
-fielname = 'user_info.json'
-with open(fielname) as f_obj:
-    username = json.load(f_obj)
-    print(username)
+def get_stored_username():
+    """Get user name if it true"""
+    fielname = 'user_info.json'
+    try:
+        with open(fielname) as f_obj:
+            username=json.load(f_obj)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+def greet_user ():
+    """Use save user if file not found we will create new file and write user name"""
+    fielname = 'user_info.json'
+    username= get_stored_username()
+    if username:
+        print('Well come back {}'.format(username))
+    else:
+        with open(fielname,'w') as f_obj:
+
+            name = input('Enter you name: ')
+            json.dump(name,f_obj)
+
--- remember_me.py
+++ remember_me.py
@@ -1,12 +1,2 @@
-import json
-fielname = 'user_info.json'
-try:
-    with open(fielname) as f_obj:
-        username = json.load(f_obj)
-        print('Well come back {}'.format(username))
-except FileNotFoundError:
-    with open(fielname,'w') as f_obj:
-        name = input('Enter you name: ')
-        json.dump(name,f_obj)
-
-
+import greet_user
+greet_user.greet_user()
