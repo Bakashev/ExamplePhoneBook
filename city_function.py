a304802cd1d6e22b0959ba70159e62097089ccb0
Bakashev Sergey <BakashevSergey@gmail.com>
2022-07-14 12:25:07 +0300

    final code with two unittest format city_coutry format city_country_population

--- city_function.py
+++ city_function.py
@@ -1,4 +1,7 @@
-def get_full_city_name(city, country, population):
-    full_name= city + ', ' +country+ ' - ' + str(population)
+def get_full_city_name(city, country, population = 0):
+    if population == 0:
+        full_name = city + ', ' +country
+    else:
+        full_name= city + ', ' +country+ ' - ' + str(population)
     return full_name.title()
 
--- test/test_city.py
+++ test/test_city.py
@@ -6,3 +6,7 @@ class TestCityCase (unittest.TestCase):
     def test_format_city_country(self):
         format_city = get_full_city_name('minsk','belarus')
         self.assertEqual(format_city, 'Minsk, Belarus')
+
+    def test_format_city_country_population(self):
+        format_name_city=get_full_city_name('minsk','belarus', 30000000)
+        self.assertEqual(format_name_city,'Minsk, Belarus - 30000000')
