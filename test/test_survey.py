185d9bffa38084b6a5694d61f6243047ec0af3b0
Bakashev Sergey <BakashevSergey@gmail.com>
2022-07-14 17:32:21 +0300

    correct test, use method setUp

--- test/test_survey.py
+++ test/test_survey.py
@@ -3,20 +3,20 @@ from survey import AnonymousSurvey
 
 class TestAnonymousSurvey(unittest.TestCase):
     """Tests for class AnonymnousSurvey"""
+    def setUp(self):
+        question = 'What is your name'
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['Sergey', 'Alina', 'Dasha']
+
     def test_store_single_response(self):
         """One response save try"""
-        question = 'What is your name'
-        my_survey=AnonymousSurvey(question)
-        my_survey.store_response('Sergey')
-        self.assertIn('Sergey',my_survey.response)
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0],self.my_survey.response)
 
     def test_store_three_response(self):
         """Three response save true"""
-        qustion = 'What is your name and names your family'
-        my_survey = AnonymousSurvey(qustion)
-        responses = ['Sergey','Alina','Dasha']
-        for response in responses:
-            my_survey.store_response(response)
-        for response in my_survey.response:
-            self.assertIn(response, my_survey.response)
+        for response in self.responses:
+            self.my_survey.store_response(response)
+        for response in self.my_survey.response:
+            self.assertIn(response, self.my_survey.response)
 
